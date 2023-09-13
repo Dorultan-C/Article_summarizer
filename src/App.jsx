290b732ef5fb7e978646fc9cb0bc1dfa0539484d
Cristian Dorultan <c.dorultan@gmail.com>
2023-09-13 22:41:54 +0100

    construct base project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <button>
+      <a href="https://www.youtube.com/watch?v=A6g8xc0MoiY" target="_blank" >Tutorial</a>
+    </button>
+    
+    <div>App</div>
+    </>
+    
+  )
+}
+
+export default App
