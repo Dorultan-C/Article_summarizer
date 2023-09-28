4741eaed43d97afc1b2892271d7629f2539df341
Cristian Dorultan <c.dorultan@gmail.com>
2023-09-28 12:46:46 +0100

    submit functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,23 +3,38 @@ import{useState, useEffect } from 'react';
 import {copy, linkIcon, loader, tick} from '../assets';
 
 const Demo = () => {
+
+  const [article, setArticle] = useState ({
+    url:'',
+    summary:''
+  });
+
+  const handleSubmit= async(e) => {
+    alert('Submitted');
+  }
+
   return (
     <section className='mt-16 w-full max-w-xl'>
       <div className='flex flex-col w-full gap-2'>
         <form 
         className='relative flex justify-centre items-centre'
-        onSubmit={ () => {}}>
+        onSubmit={ handleSubmit }>
+
           <img 
           src={linkIcon} 
           alt="link-icon"
           className='absolute left-0 my-2 ml-3 w-5' />
+
           <input 
           type="url"
           placeholder='Please enter a URL'
-          value=""
-          onChange={ () => {} }
+          value= {article.url}
+          onChange={(e) => setArticle ({
+            ...article, url: e.target.value
+          })}
           required
           className='url_input peer' />
+
           <button 
           type='submit'
           className='submit_btn
@@ -27,6 +42,7 @@ const Demo = () => {
                      peer-focus:text-gray-700'>
             ↵
           </button>
+
         </form>
       </div>
     </section>
